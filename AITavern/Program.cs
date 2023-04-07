f5b528db5e36e10dfe5dd731997149f239036c85
Todd Schluechtermann <dyson_swarm@outlook.com>
2023-04-07 09:14:55 -0500

    Remove unneeded stuff

--- AITavern/Program.cs
+++ AITavern/Program.cs
@@ -10,8 +10,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddOidcAuthentication(options =>
 {
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
 
